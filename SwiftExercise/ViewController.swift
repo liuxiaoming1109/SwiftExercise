7b889b7b8c3fef537576191bd7e2d3e00ef77af3
liuxm <liuxiaoming1109@sina.com>
2015-08-06 11:24:06 +0800

    Initial Commit

---
+++ SwiftExercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftExercise
+//
+//  Created by liuxm on 15/8/6.
+//  Copyright (c) 2015年 evan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
