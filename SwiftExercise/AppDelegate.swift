c67e978f24128d6692d0939a91a201c82b7eb840
liuxm <liuxiaoming1109@sina.com>
2016-07-05 09:29:42 +0800

    修改

--- SwiftExercise/AppDelegate.swift
+++ SwiftExercise/AppDelegate.swift
@@ -7,16 +7,64 @@
 //
 
 import UIKit
+import AVOSCloud
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // 远程推送
+        self.avOSCloudPushInit(application, launchOptions: launchOptions)
+
         return true
+    }
+    
+    /**
+     注册成功，APNs 会返回给你设备的 token，iOS 系统会把它传递给 app delegate 代理
+     
+     - parameter application: application
+     - parameter deviceToken: deviceToken
+     */
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        print("Receive DeviceToken: %@", deviceToken)
+        // token 保存到 AVOS Cloud 后台
+        let currentInstallation = AVInstallation.currentInstallation()
+        currentInstallation.setDeviceTokenFromData(deviceToken)
+        currentInstallation.saveInBackground()
+    }
+    
+    /**
+     注册失败
+     
+     - parameter application: application
+     - parameter error:       error
+     */
+    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+        print("注册失败，无法获取设备 ID, 具体错误: %@", error);
+    }
+    
+    /**
+     如果远程消息发送过来的时候，app 正在运行, 同时远程消息中的 payload 数据会作为参数传递进去
+     
+     - parameter application: application
+     - parameter userInfo:    userInfo
+     */
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        if application.applicationState == UIApplicationState.Active {
+            // 转换成一个本地通知，显示到通知栏，你也可以直接显示出一个 alertView，只是那样稍显 aggressive：
+            let localNotification = UILocalNotification()
+            localNotification.userInfo = userInfo
+            localNotification.soundName = UILocalNotificationDefaultSoundName
+            localNotification.alertBody = userInfo["aps"]?.objectForKey("alert") as? String
+            localNotification.fireDate = NSDate()
+            UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
+        } else {
+            AVAnalytics.trackAppOpenedWithRemoteNotificationPayload(userInfo)
+        }
     }
 
     func applicationWillResignActive(application: UIApplication) {
@@ -40,7 +88,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
+    /**
+     使用AVOSCloud 进行远程推送
+     */
+    func avOSCloudPushInit(application: UIApplication, launchOptions: [NSObject: AnyObject]?) {
+        // 使用AVOSCloud管理远程推送
+        AVOSCloud.setApplicationId("MP9Oqipt5Rkt5PIb8HYch5YF", clientKey: "gmjP9UItzQco4Fb90zqCA74J")
+        
+        AVOSCloud.setLastModifyEnabled(true)
+        AVOSCloud.registerForRemoteNotification()
+        
+        application.applicationIconBadgeNumber = 0
+        
+        // 注册推送通知
+        if #available(iOS 8.0, *) {
+            application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: [UIUserNotificationType.Alert, UIUserNotificationType.Badge, UIUserNotificationType.Sound], categories: nil))
+            application.registerForRemoteNotifications()
+        } else {
+            // Fallback on earlier versions
+            application.registerForRemoteNotificationTypes([UIRemoteNotificationType.Alert, UIRemoteNotificationType.Badge, UIRemoteNotificationType.Sound])
+        }
+        
+        if (launchOptions != nil) {
+            AVAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        }
+    }
+    
 
 }
 
--- SwiftExercise/ViewController/自动布局/基础/AutoLayoutTableTableViewCell.swift
+++ SwiftExercise/ViewController/自动布局/基础/AutoLayoutTableTableViewCell.swift
@@ -14,6 +14,14 @@ class AutoLayoutTableTableViewCell: UITableViewCell {
     
     @IBOutlet weak var label: UILabel!
     
+    init(style: UITableViewCellStyle, reuseIdentifier: String?, assd:String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
